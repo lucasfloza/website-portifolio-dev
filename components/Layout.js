96e642cea0917fad6a126ec08dd4b27fd78d24ce
Lucas <lucasfsouza.l10@gmail.com>
2023-10-10 10:52:49 -0300

    implementing layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,23 @@
 //fonts
-import {Sora} from '@next/font/google'
+import { Sora } from "@next/font/google";
 
 //font settings
 const sora = Sora({
-  subsets: ['latin'],
-  variable: '--font-sora',
-  weight: ['100','200','300','400','500','600','700','800']
-})
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 //components
-import Nav from '../components/Nav'
-import Header from '../components/Header'
-import TopLeftImg from '../components/TopLeftImg'
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImg from "../components/TopLeftImg";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <div>
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable}`}
+    >
       <TopLeftImg />
       <Nav />
       <Header />
