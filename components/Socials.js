c5cd0ab1daa5bd39d21290d39108f639e670e752
Lucas <lucasfsouza.l10@gmail.com>
2023-10-10 10:51:18 -0300

    implementing socials component

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,35 @@
+//next link
+import Link from "next/link";
+
+// icons
+import {RiYoutubeLine, RiInstagramLine, RiFacebookLine, RiDribbbleLine, RiBehanceLine,RiPinterestLine,RiMediumLine} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+  <div className="flex items-center gap-x-5 text-lg">
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiYoutubeLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiInstagramLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiFacebookLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiDribbbleLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiBehanceLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiPinterestLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiMediumLine />
+    </Link>
+  </div>
+  );
 };
 
 export default Socials;
